9204b9d35c644ca4d20cebee871ffbdaf598de72
tck8888 <l>
2019-10-11 22:53:19 +0800

    搭建SpringBoot开发环境

---
+++ apkresourcemanage/src/main/java/com/tck/apkresourcemanage/ApkresourcemanageApplication.java
@@ -0,0 +1,13 @@
+package com.tck.apkresourcemanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApkresourcemanageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApkresourcemanageApplication.class, args);
+    }
+
+}
---
+++ apkresourcemanage/src/test/java/com/tck/apkresourcemanage/ApkresourcemanageApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tck.apkresourcemanage;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApkresourcemanageApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
